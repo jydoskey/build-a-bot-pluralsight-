1841ba9877eabd3c8659e9c04f4d04c1edef0ffc
jide_omotola <jide@reach.africa>
2020-04-05 13:25:52 +0100

    removed some default styles and renamed default vue file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <home-page />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HomePage from './components/HomePage.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      HomePage
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background: linear-gradient(to bottom, #555, #999);
+    background-attachment: fixed;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+  }
+
+  main {
+    margin: 0 auto;
+    padding: 30px;
+    background-color: white;
+    width: 1024px;
+    min-height: 300px;
+  }
 </style>
