1ba9da2dffe0361eb9e9e434378f3265d01af8fa
jide_omotola <jide@reach.africa>
2020-04-05 15:04:26 +0100

    added top navigation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item"><img class="logo" src="@/assets/build-a-bot-logo.png">Build-a-Bot</li>
+        </ul>
+      </nav>
+    </header>
     <main>
       <home-page />
     </main>
@@ -7,7 +14,7 @@
 </template>
 
 <script>
-  import HomePage from './components/HomePage.vue'
+  import HomePage from '@/home/HomePage.vue'
 
   export default {
     name: 'App',
@@ -33,5 +40,28 @@
     background-color: white;
     width: 1024px;
     min-height: 300px;
+  }
+
+  header {
+    background-color: #999;
+    width: 1084px;
+    margin: 0 auto;
+  }
+
+  ul {
+    padding: 3px;
+    display: flex;
+  }
+
+  .nav-item {
+    display: inline-block;
+    padding: 5px 10px;
+    font-size: 22px;
+    border-right: 1px solid #bbb;
+  }
+
+  .logo {
+    vertical-align: middle;
+    height: 30px;
   }
 </style>
