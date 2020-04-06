d7fb7816501d5ff67940a09c2b554ea82840b8b5
jide_omotola <jide@reach.africa>
2020-04-06 10:52:54 +0100

    collapsible com[onent used to hide and display add cart element]

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,25 +1,38 @@
 <template>
   <div class="content">
-    <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
+    <div class="preview"> 
+      <CollapsibleSection>
+      <div class="preview-content">
+        <div class="top-row">
+          <img :src="selectedRobot.head.src"/>
+        </div>
+        <div class="middle-row">
+          <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
+          <img :src="selectedRobot.torso.src"/>
+          <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
+        </div>
+        <div class="bottom-row">
+          <img :src="selectedRobot.base.src"/>
+        </div>
+      </div>
+       </CollapsibleSection>
+          <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
+
+    </div>
     <div class="top-row">
       <div class="top part" :class="{'sale-border': selectedRobot.head.onSale}">
         <div class="robot-name">{{selectedRobot.head.title}}<span v-if="selectedRobot.head.onSale" class="sale">
             Sale!</span></div>
-        <PartSelector :parts="availableParts.heads"
-        position="top"/>
+        <PartSelector :parts="availableParts.heads" position="top" @partSelected="part => selectedRobot.head=part" />
       </div>
     </div>
     <div class="middle-row">
-      <PartSelector :parts="availableParts.arms"
-      position="left"/>
-      <PartSelector :parts="availableParts.torsos"
-      position="center"/>
-      <PartSelector :parts="availableParts.arms"
-      position="right"/>
+      <PartSelector :parts="availableParts.arms" position="left" @partSelected="part => selectedRobot.leftArm=part" />
+      <PartSelector :parts="availableParts.torsos" position="center" @partSelected="part => selectedRobot.torso=part" />
+      <PartSelector :parts="availableParts.arms" position="right" @partSelected="part => selectedRobot.rightArm=part" />
     </div>
     <div class="bottom-row">
-      <PartSelector :parts="availableParts.bases"
-      position="bottom"/>
+      <PartSelector :parts="availableParts.bases" position="bottom" @partSelected="part => selectedRobot.base=part" />
     </div>
     <div>
       <h1>Cart</h1>
@@ -43,11 +56,15 @@
 
 <script>
   import availableParts from '@/data/parts';
-  import PartSelector from '@/build/PartSelector'
+  import PartSelector from '@/build/PartSelector';
+  import CollapsibleSection from '@/shared/CollapsibleSection';
 
   export default {
     name: "RobotBuilder",
-    components: {PartSelector},
+    components: {
+      PartSelector,
+      CollapsibleSection
+    },
     data() {
       return {
         availableParts,
@@ -58,22 +75,24 @@
           torso: {},
           rightArm: {},
           base: {}
-      }
+        }
       }
     },
     computed: {
       headBorderStyle() {
         return {
-          border: this.selectedRobot.head.onSale ? "3px solid red": "3px solid #aaa",
+          border: this.selectedRobot.head.onSale ? "3px solid red" : "3px solid #aaa",
         };
       },
-      
+
     },
     methods: {
       addToCart() {
         const robot = this.selectedRobot;
         const cost = robot.head.cost + robot.leftArm.cost + robot.torso.cost + robot.rightArm.cost + robot.base.cost;
-        this.cart.push(Object.assign({}, robot, {cost}));
+        this.cart.push(Object.assign({}, robot, {
+          cost
+        }));
       },
     }
   }
@@ -202,22 +221,47 @@
   .content {
     position: relative;
   }
+
   .add-to-cart {
     position: absolute;
-    right: 30px;
-    width: 220px;
+    width: 210px;
     padding: 2px;
     font-size: 16px;
   }
-  td, th {
+
+  td,
+  th {
     text-align: left;
     padding: 5px;
     padding-right: 20px;
   }
+
   .cost {
     text-align: right;
   }
+
   .sale-border {
     border: 3px solid red;
   }
+  .preview {
+  position: absolute;
+  top: -20px;
+  right: 0;
+  width: 210px;
+  height: 210px;
+  padding: 5px;
+}
+.preview-content {
+  border: 1px solid #999;
+}
+.preview img {
+  width: 50px;
+  height: 50px;
+}
+.rotate-right {
+  transform: rotate(90deg);
+}
+.rotate-left {
+  transform: rotate(-90deg);
+}
 </style>
