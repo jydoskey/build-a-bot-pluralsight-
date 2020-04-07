eec16e8d36734b292e57f55022e7195e69b4f8e9
jide_omotola <jide@reach.africa>
2020-04-07 01:40:01 +0100

    created new files, worked with nested routes and named views

--- src/parts/PartInfo.vue
+++ src/parts/PartInfo.vue
@@ -8,13 +8,24 @@
   import parts from '@/data/parts';
   export default {
     name: 'PartInfo',
+    props: {
+      partType: {
+        type: String
+      },
+      id: {
+        type: [Number, String],
+        validator(value) {
+          return Number.isInteger(Number(value));
+        },
+      },
+    },
     computed: {
       part() {
         const {
           partType,
           id
-        } = this.$route.params;
-        return parts[partType].find((part) => part.id === + id);
+        } = this;
+        return parts[partType].find((part) => part.id === +id);
       },
     },
   };
