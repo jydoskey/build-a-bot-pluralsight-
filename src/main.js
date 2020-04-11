1c0d357bd7c4282a268e389b66d52144e3de7e4c
jide_omotola <jide@reach.africa>
2020-04-12 00:17:43 +0100

    filter declared globally

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue';
 
 import router from '@/router';
 import store from '@/store';
+import currencyFilter from './shared/currency-filter';
+Vue('currency', currencyFilter);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
